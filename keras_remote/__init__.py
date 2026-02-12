d633efa5bfecd7f1e593dbbddbee8f8338bc30c8
Divyashree Sreepathihalli <divyashreepathihalli@gmail.com>
2026-02-12 08:51:12 -0800

    Reorganize code (#16)
    * update project structure
    
    * update high level code structure to keras style
    
    * copy this init file from keras

--- keras_remote/__init__.py
+++ keras_remote/__init__.py
@@ -1,1 +1,12 @@
-from keras_remote.core import run
+# This file should NEVER be packaged! This is a hack to make "import keras_remote" from
+# the base of the repo just import the source files. We'll keep it for compat.
+
+import os  # isort: skip
+
+# Add everything in /api/ to the module search path.
+__path__.append(os.path.join(os.path.dirname(__file__), "api"))  # noqa: F405
+
+from keras_remote.api import *  # noqa: F403, E402
+
+# Don't pollute namespace.
+del os
--- keras_remote/src/core/core.py
+++ keras_remote/src/core/core.py
@@ -6,16 +6,18 @@ import os
 import shutil
 import tempfile
 
-from keras_remote import packager
-from keras_remote import infra
-from keras_remote.execution import (
+from keras_remote.src.utils import packager
+from keras_remote.src.infra import infra
+from keras_remote.src.backend.execution import (
     JobContext,
     execute_remote,
     GKEBackend,
 )
+from keras_remote.src.api_export import keras_remote_export
 
 logger = infra.logger
 
+@keras_remote_export("keras_remote.run")
 def run(accelerator="v3-8",
         container_image=None,
         zone=None,
