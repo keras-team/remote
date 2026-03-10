6a68932cc2d50f63ccfec8914433d5bd683dc2e0
Jyotinder Singh <33001894+JyotinderSingh@users.noreply.github.com>
2026-03-10 13:11:42 +0530

    address reviews

--- keras_remote/__init__.py
+++ keras_remote/__init__.py
@@ -2,7 +2,6 @@ import os
 
 # Suppress noisy gRPC fork/logging messages before any gRPC imports
 os.environ.setdefault("GRPC_VERBOSITY", "NONE")
-os.environ.setdefault("GLOG_minloglevel", "3")
 os.environ.setdefault("GRPC_ENABLE_FORK_SUPPORT", "0")
 
 import logging as python_logging
